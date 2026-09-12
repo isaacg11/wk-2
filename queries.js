5c481a46b708b6838ea434059e91983bb8b6f1c1
Isaac Grey <isaac.j.grey@gmail.com>
2026-09-12 09:13:14 -0700

    Added queries

--- queries.js
+++ queries.js
@@ -1,36 +1,90 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import User from "./models/User.js"
-import Subreddit from './models/Subreddit.js';
-import Thread from './models/Thread.js';
+import User from "./models/User.js";
+import Subreddit from "./models/Subreddit.js";
+import Thread from "./models/Thread.js";
 
-
+// 1. Find user by email diana@example.com
 async function query1() {
-    // Write code for Query 1 here
+  const user = await User.findOne({ email: "diana@example.com" });
+  console.log(user);
 }
 
+// 2. Get threads in a subreddit programming
 async function query2() {
-    // Write code for Query 2 here
+  const subreddit = await Subreddit.findOne({ name: "programming" });
+  const threads = await Thread.find({ subreddit: subreddit._id });
+  console.log(threads);
 }
 
+// 3. Users who posted threads
 async function query3() {
-    // Write code for Query 3 here
+  const userIds = await Thread.distinct("author");
+  const users = await User.find({ _id: { $in: userIds } });
+  console.log(users);
 }
 
+// 4. Threads created on or after January 1, 2024
 async function query4() {
-    // Write code for Query 4 here
+  const threads = await Thread.find({
+    createdAt: { $gte: new Date("2024-01-01") },
+  });
+
+  console.log(threads);
 }
 
-// more queries
+// 5. Add a new thread (Create) Subreddit devops author Ethan
+async function query5() {
+  const subreddit = await Subreddit.findOne({ name: "devops" });
+  const author = await User.findOne({ name: "Ethan" });
+
+  const thread = await Thread.create({
+    title: "Docker or kubernetes?",
+    content: "Which one is better?",
+    author: author._id,
+    subreddit: subreddit._id,
+    createdAt: new Date(),
+  });
+
+  console.log(thread);
+}
+
+// 6. Update title of the thread "Docker and kubernetes?
+async function query6() {
+  const thread = await Thread.findOne({ title: "Docker or kubernetes?" });
+  thread.title = "Facebook or Snapchat?";
+  await thread.save();
+
+  console.log(thread);
+}
+
+// 7. Delete all subreddits and their associated threads
+async function query7() {
+  const subreddits = await Subreddit.find({});
+
+  for (const subreddit of subreddits) {
+    const deletedThreads = await Thread.deleteMany({
+      subreddit: subreddit._id,
+    });
+
+    console.log(deletedThreads.deletedCount);
+  }
+
+  await Subreddit.deleteMany({});
+  console.log("deleted all subreddits and their associated threads");
+}
 
 async function runQueries() {
-    // Uncomment the query you want to run
-    // await query1();
-    // await query2();
-    // await query3();
-    // await query4();
-    // more
+  // Uncomment the query you want to run
+  // await query1();
+  // await query2();
+  // await query3();
+  // await query4();
+  // await query5();
+  // await query6();
+  await query7();
+  // more
 }
 
 async function main() {
